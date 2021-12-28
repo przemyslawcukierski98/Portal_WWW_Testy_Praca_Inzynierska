ab096eeb307f9d211313b08201ca563bc6a5264d
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2021-12-28 10:33:39 +0100

    Dodanie filtra wyszukiwania w widoku wyświetlającym egzaminy ocenione (student)

--- OnlineTesty_Library/Repositories/StudentTestResultRepositories.cs
+++ OnlineTesty_Library/Repositories/StudentTestResultRepositories.cs
@@ -14,20 +14,59 @@ namespace OnlineTesty_Library.Repositories
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IStudentAndGroupRepositories _studentAndGroupRepositories;
+        private readonly ILecturerProfileDetailsRepositories _lecturerProfileDetailsRepositories;
         
-
         public StudentTestResultRepositories(IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor,
-            IStudentAndGroupRepositories studentAndGroupRepositories) : base(unitOfWork)
+            IStudentAndGroupRepositories studentAndGroupRepositories,
+            ILecturerProfileDetailsRepositories lecturerProfileDetailsRepositories) : base(unitOfWork)
         {
             _httpContextAccessor = httpContextAccessor;
             _studentAndGroupRepositories = studentAndGroupRepositories;
+            _lecturerProfileDetailsRepositories = lecturerProfileDetailsRepositories;
         }
 
-        public IEnumerable<StudentTestResult> FindEvaluatedExamsForStudent()
+        public IEnumerable<StudentTestResult> FindEvaluatedExamsForStudent(string titleFilter, string lecturerFilter)
         {
-            var studentEmail = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Email).ToString().Substring(67).Trim();
+            IQueryable<StudentTestResult> evaluatedExams = Enumerable.Empty<StudentTestResult>().AsQueryable();
+            LecturerProfileDetails lecturerProfileDetails = new LecturerProfileDetails();
+            string studentEmail = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Email).ToString().Substring(67).Trim();
+            string lecturerEmail = string.Empty;
 
-            return this.GetDbSet<StudentTestResult>().Where(e => e.StudentEmail == studentEmail);
+            if (titleFilter == null) titleFilter = string.Empty;
+            if (lecturerFilter == null) lecturerFilter = string.Empty;
+
+            lecturerProfileDetails = _lecturerProfileDetailsRepositories.ReadByLastName(lecturerFilter);
+
+            if (lecturerProfileDetails != null)
+            {
+                lecturerEmail = lecturerProfileDetails.EmailAddress;
+            }
+
+            // walidacja
+            ValidationObject validation = ValidationMethods.ValidationForExams(titleFilter, lecturerFilter, null);
+
+            if (validation.TitleFilterIsFilled && validation.StudentFilterIsFilled)
+            {
+                evaluatedExams = this.GetDbSet<StudentTestResult>().Where(e => e.LecturerEmail == lecturerEmail)
+                .Where(e => e.ExamTitle.Contains(titleFilter))
+                .Where(e => e.StudentEmail == studentEmail);
+            }
+            else if (validation.TitleFilterIsFilled && validation.StudentFilterIsNullOrEmpty)
+            {
+                evaluatedExams = this.GetDbSet<StudentTestResult>().Where(e => e.StudentEmail == studentEmail)
+                .Where(e => e.ExamTitle.Contains(titleFilter));
+            }
+            else if (validation.TitleFilterIsNullOrEmpty && validation.StudentFilterIsFilled)
+            {
+                evaluatedExams = this.GetDbSet<StudentTestResult>().Where(e => e.LecturerEmail == lecturerEmail)
+                .Where(e => e.StudentEmail == studentEmail);
+            }
+            else
+            {
+                evaluatedExams = this.GetDbSet<StudentTestResult>().Where(e => e.StudentEmail == studentEmail);
+            }
+
+            return evaluatedExams;
         }
 
         public IEnumerable<StudentTestResult> FindEvaluatedExamsForLecturer(string titleFilter, string studentFilter)
@@ -92,7 +131,7 @@ namespace OnlineTesty_Library.Repositories
     {
         StudentTestResult Read(Guid? ID);
         Guid SaveExamResult(StudentTestResult model);
-        IEnumerable<StudentTestResult> FindEvaluatedExamsForStudent();
+        IEnumerable<StudentTestResult> FindEvaluatedExamsForStudent(string titleFilter, string lecturerFilter);
         IEnumerable<StudentTestResult> FindEvaluatedExamsForLecturer(string titleFilter, string studentFilter);
     }
 }
